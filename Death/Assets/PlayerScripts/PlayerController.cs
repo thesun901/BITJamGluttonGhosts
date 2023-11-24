fc7b4d011dc6d612b59b866e6b4ee8a0db4b5687
jakubik307 <and.jj.an@gmail.com>
2023-11-24 21:11:54 +0100

    basic movement

---
+++ Death/Assets/PlayerScripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int healthPoints;
+    [SerializeField] private float speed;
+    private bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float moveInputHorizontal = Input.GetAxis("Horizontal");
+        float moveInputVertical = Input.GetAxis("Vertical");
+        Vector2 velocity = new Vector2(moveInputHorizontal * speed * Time.deltaTime, moveInputVertical * speed * Time.deltaTime);
+
+        if (!isDead)
+        {
+            transform.Translate(velocity);
+        }
+
+    }
+
+    void CheckIfDead()
+    {
+        isDead = healthPoints <= 0;
+    }
+}
