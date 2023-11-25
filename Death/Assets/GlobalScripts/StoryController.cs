244f91a6db6c25b2b5546bf455bb85749652293e
jakubik307 <and.jj.an@gmail.com>
2023-11-25 16:30:51 +0100

    add story scene

---
+++ Death/Assets/GlobalScripts/StoryController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StoryController : MonoBehaviour
+{
+    public AudioSource storyAudio;
+    [SerializeField] private AudioClip wooshSound;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private float timePerLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        storyAudio = GetComponent<AudioSource>();
+
+        List<string> list = new List<string>();
+        list.Add("daskdmaskd");
+        list.Add("test");
+        list.Add("daskdmaskd");
+        list.Add("test");
+        list.Add("daskdmaskd");
+
+        PrintText(list);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PrintText(List<string> textList)
+    {
+        StartCoroutine(PrintTextWithDelay(textList));
+    }
+
+    IEnumerator PrintTextWithDelay(List<string> textList)
+    {
+        foreach (string line in textList)
+        {
+            PrintLine(line);
+            yield return new WaitForSeconds(timePerLine);
+        }
+    }
+
+    void PrintLine(string textLine)
+    {
+        storyAudio.PlayOneShot(wooshSound);
+        text.text = textLine;
+    }
+}
